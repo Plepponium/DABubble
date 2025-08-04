8e060c3e01ca356c983e8c4d70ad1d39c5bb01f8
Plepponium <kleinchmida@gmail.com>
2025-08-04 16:25:01 +0200

    change login + add font

--- dabubble/src/app/login/login.component.ts
+++ dabubble/src/app/login/login.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatCardModule, MatFormFieldModule, MatButtonModule, MatInputModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
