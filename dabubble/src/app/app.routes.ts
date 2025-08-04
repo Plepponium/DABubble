8c1344db4654ca2e7022912aef0d6754e327b1c6
Plepponium <kleinchmida@gmail.com>
2025-08-04 22:29:42 +0200

    add legal-links to login

--- dabubble/src/app/app.routes.ts
+++ dabubble/src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'main', component: MainPageComponent, },
     { path: 'signup', component: SignupComponent, },
+    { path: 'imprint', component: ImprintComponent, },
+    { path: 'privacy', component: PrivacyComponent, },
 
 ];
