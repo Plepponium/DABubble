cbf87e0c14b0150354f5101425187597605eaa86
Plepponium <kleinchmida@gmail.com>
2025-08-04 18:53:42 +0200

    create signup + routerLink in login

--- dabubble/src/app/login/login.component.ts
+++ dabubble/src/app/login/login.component.ts
@@ -3,10 +3,11 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [MatCardModule, MatFormFieldModule, MatButtonModule, MatInputModule],
+  imports: [MatCardModule, MatFormFieldModule, MatButtonModule, MatInputModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
