2ae8bae4dce48b77849d4e996d9d342887bd65e7
CarlaHoffmann <carlahoffmann@gmx.de>
2025-08-04 14:45:59 +0200

    added style to menu

--- dabubble/src/app/menu/menu.component.ts
+++ dabubble/src/app/menu/menu.component.ts
@@ -1,9 +1,12 @@
 import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
 import {MatExpansionModule} from '@angular/material/expansion';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-menu',
-  imports: [MatExpansionModule],
+  imports: [MatExpansionModule, MatIconModule, MatButtonModule, MatDividerModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
