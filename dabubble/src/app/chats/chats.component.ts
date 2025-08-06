286f646c3b6f369fc6d528630b3989f404f91ff5
CarlaHoffmann <carlahoffmann@gmx.de>
2025-08-06 12:30:22 +0200

    added chats input styling

--- dabubble/src/app/chats/chats.component.ts
+++ dabubble/src/app/chats/chats.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-chats',
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule],
   templateUrl: './chats.component.html',
   styleUrl: './chats.component.scss'
 })
 export class ChatsComponent {
-
+  value = 'Clear me';
 }
