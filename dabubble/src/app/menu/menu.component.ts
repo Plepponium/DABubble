b7478af41870e4bdc0c6ec603349e496b861bcba
CarlaHoffmann <carlahoffmann@gmx.de>
2025-08-06 17:03:29 +0200

    added menu expanding

--- dabubble/src/app/menu/menu.component.ts
+++ dabubble/src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import { CommonModule } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -6,11 +7,26 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-menu',
-  imports: [MatIconModule, MatSidenavModule, MatButtonModule, MatToolbarModule],
+  imports: [CommonModule, MatIconModule, MatSidenavModule, MatButtonModule, MatToolbarModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MenuComponent {
   readonly panelOpenState = signal(false);
+  channelsExpanded = true;
+  usersExpanded = true;
+
+  toggleChannels() {
+    this.channelsExpanded = !this.channelsExpanded;
+  }
+
+  addChannel(event: Event) {
+    event.stopPropagation(); // Verhindert, dass das click-Event der list-header-container ausgelöst wird
+    // Logik zum Hinzufügen eines Channels
+  }
+
+  toggleUsers() {
+    this.usersExpanded = !this.usersExpanded;
+  }
 }
