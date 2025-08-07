5754d0362d7e52f18a26c1e8d8c871b1ba87e917
CarlaHoffmann <carlahoffmann@gmx.de>
2025-08-07 15:27:12 +0200

    added thread

--- dabubble/src/app/thread/thread.component.ts
+++ dabubble/src/app/thread/thread.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-thread',
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule],
   templateUrl: './thread.component.html',
   styleUrl: './thread.component.scss'
 })
