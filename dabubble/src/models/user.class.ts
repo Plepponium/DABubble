af4545f0e11580c5d97a2e7c3dd68983a36c1a57
Plepponium <kleinchmida@gmail.com>
2025-08-12 15:11:30 +0200

    add firebase + config, update user model

--- dabubble/src/models/user.class.ts
+++ dabubble/src/models/user.class.ts
@@ -1,9 +1,21 @@
-export interface User {
+export class User {
     uid: string;
-    displayName: string;
+    name: string;
     email: string;
-    photoURL?: string;
-    createdAt?: any;
-    lastSeen?: any;
-    presence?: 'online' | 'offline' | 'idle';
+    password: string;
+    photoURL: string;
+    createdAt: any;
+    lastSeen: any;
+    presence: 'online' | 'offline' | 'idle';
+
+    constructor(obj?: any) {
+        this.uid = obj ? obj.uid : '';
+        this.name = obj ? obj.name : '';
+        this.email = obj ? obj.email : '';
+        this.password = obj ? obj.password : '';
+        this.photoURL = obj ? obj.photoURL : '';
+        this.createdAt = obj ? obj.createdAt : '';
+        this.lastSeen = obj ? obj.lastSeen : '';
+        this.presence = obj ? obj.presence : '';
+    }
 }
