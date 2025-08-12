66c4048476f592aa9b5613690c215cdf82bfd7c6
CarlaHoffmann <carlahoffmann@gmx.de>
2025-08-12 16:52:05 +0200

    added thread styling

--- dabubble/src/app/thread/thread.component.ts
+++ dabubble/src/app/thread/thread.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
@@ -8,10 +9,14 @@ import { RoundBtnComponent } from '../round-btn/round-btn.component';
 
 @Component({
   selector: 'app-thread',
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, RoundBtnComponent],
+  imports: [CommonModule, FormsModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, RoundBtnComponent],
   templateUrl: './thread.component.html',
   styleUrl: './thread.component.scss'
 })
 export class ThreadComponent {
+  editCommentDialogueExpanded = false;
 
+  openEditCommentDialogue() {
+    this.editCommentDialogueExpanded = !this.editCommentDialogueExpanded;
+  }
 }
