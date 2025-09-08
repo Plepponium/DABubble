5bd5d8018eabe8d61bf34ab29a1ecba355ae4ca4
Plepponium <kleinchmida@gmail.com>
2025-09-08 21:41:49 +0200

    change channel model

--- dabubble/src/models/channel.class.ts
+++ dabubble/src/models/channel.class.ts
@@ -1,24 +1,26 @@
 export class Channel {
-    id!: string;
-    name: string = '';
-    user: string = '';
-    createdAt?: any;
-    lastMessage?: any;
-    chats?: string;
+  id!: string;
+  name: string = '';
+  description?: string;
+  user: string = '';
+  createdBy?: string;
+  createdAt?: any;
+  lastMessage?: any;
+  chats?: string;
 
-    constructor(init?: Partial<Channel>) {
+  constructor(init?: Partial<Channel>) {
     Object.assign(this, init);
   }
 
-//   static fromFirestore(doc: any): Channel {
-//     const data = doc.data ? doc.data() : doc;
-//     return new Channel({
-//       id: doc.id ?? data.id,
-//       name: data.name ?? '',
-//       user: data.user ?? '',
-//       createdAt: data.createdAt,
-//       lastMessage: data.lastMessage,
-//       chats: data.chats
-//     });
-//   }
+  //   static fromFirestore(doc: any): Channel {
+  //     const data = doc.data ? doc.data() : doc;
+  //     return new Channel({
+  //       id: doc.id ?? data.id,
+  //       name: data.name ?? '',
+  //       user: data.user ?? '',
+  //       createdAt: data.createdAt,
+  //       lastMessage: data.lastMessage,
+  //       chats: data.chats
+  //     });
+  //   }
 }
