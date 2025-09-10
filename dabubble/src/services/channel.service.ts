1e7a72d5b47984627b5cf83e1949400841af4487
CarlaHoffmann <carlahoffmann@gmx.de>
2025-09-10 14:58:04 +0200

    added channel service

--- dabubble/src/services/channel.service.ts
+++ dabubble/src/services/channel.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
-import { Firestore, collectionData, collection, addDoc } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { Firestore, collectionData, collection, doc, addDoc } from '@angular/fire/firestore';
+import { Observable, switchMap, map } from 'rxjs';
 import { Channel } from '../models/channel.class';
 
 @Injectable({ providedIn: 'root' })
@@ -14,8 +14,18 @@ export class ChannelService {
     return collectionData(channelsCollection, { idField: 'id' }) as Observable<Channel[]>;
   }
 
+  getChatsForChannel(channelId: string): Observable<any[]> {
+    const chatsCollection = collection(this.firestore, `channels/${channelId}/chats`);
+    return collectionData(chatsCollection, { idField: 'id' }) as Observable<any[]>;
+  }
+
   addChannel(channel: Partial<Channel>): Promise<any> {
     const channelsCollection = collection(this.firestore, 'channels');
     return addDoc(channelsCollection, channel);
+  }
+
+  addChatToChannel(channelId: string, chat: any): Promise<any> {
+    const chatsCollection = collection(this.firestore, `channels/${channelId}/chats`);
+    return addDoc(chatsCollection, chat);
   }
 }
