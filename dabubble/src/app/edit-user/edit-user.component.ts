2f2eb85a1dcb6b53f70dcef851b85c4259d8f1a2
Plepponium <kleinchmida@gmail.com>
2025-09-27 23:50:36 +0200

    added handleSave to edit-user

--- dabubble/src/app/edit-user/edit-user.component.ts
+++ dabubble/src/app/edit-user/edit-user.component.ts
@@ -2,10 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, Output, EventEmitter, Input, inject } from '@angular/core';
 import { RoundBtnComponent } from '../round-btn/round-btn.component';
 import { UserService } from '../../services/user.service';
+import { User } from '../../models/user.class';
+import { FormsModule } from '@angular/forms';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-edit-user',
-  imports: [CommonModule, RoundBtnComponent],
+  imports: [CommonModule, RoundBtnComponent, FormsModule],
   templateUrl: './edit-user.component.html',
   styleUrl: './edit-user.component.scss'
 })
@@ -15,6 +18,7 @@ export class EditUserComponent {
 
   currentUser$ = this.userService.getCurrentUser();
 
+  editedName: string = '';
   selectedImg?: string;
   showImgPicker = false;
   animationClass = '';
@@ -37,6 +41,26 @@ export class EditUserComponent {
   resetImg() {
     this.selectedImg = 'default-user';
   }
+
+  isNameValid(): boolean {
+    const trimmedName = this.editedName?.trim();
+    return !!trimmedName && trimmedName.split(/\s+/).length >= 2;
+  }
+
+
+  async handleSave() {
+    const user = await firstValueFrom(this.currentUser$);
+    if (!user) return;
+
+    const updatedData: Partial<User> = {
+      name: this.editedName || user.name,
+      img: this.selectedImg || user.img
+    };
+
+    await this.userService.updateUser(user.uid, updatedData);
+    this.handleClose();
+  }
+
 
   onMouseEnter() {
     // this.animationClass = 'wiggle';
