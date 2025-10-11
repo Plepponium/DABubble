81a43c652e744803efaff9428d24827cabc88995
Plepponium <kleinchmida@gmail.com>
2025-10-11 17:03:56 +0200

    hide firebase console.warn in devMode

--- dabubble/src/main.ts
+++ dabubble/src/main.ts
@@ -2,5 +2,21 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+const isDevMode =
+  window.location.hostname === 'localhost' ||
+  window.location.hostname === '127.0.0.1';
+
+// 👇 Filter für "outside injection context" Warnungen
+if (isDevMode) {
+  const originalWarn = console.warn;
+  console.warn = (...args: any[]) => {
+    const message = args.join(' ');
+    if (message.includes('outside injection context')) {
+      return; // Warnung überspringen
+    }
+    originalWarn(...args);
+  };
+}
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
