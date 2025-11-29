12e32f2e5de5216601d71d39a3c6ddef768dbc2d
Plepponium <kleinchmida@gmail.com>
2025-11-29 02:42:09 +0100

    update addChannel check for existing channelName

--- dabubble/src/app/add-channel-overlay/add-channel-overlay.component.ts
+++ dabubble/src/app/add-channel-overlay/add-channel-overlay.component.ts
@@ -1,13 +1,14 @@
-import { Component, Output, EventEmitter, inject, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter, inject, OnInit, Input } from '@angular/core';
 import { RoundBtnComponent } from '../round-btn/round-btn.component';
 import { ChannelService } from '../../services/channel.service';
 import { FormsModule } from '@angular/forms';
 import { UserService } from '../../services/user.service';
 import { User } from '../../models/user.class';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-channel-overlay',
-  imports: [RoundBtnComponent, FormsModule],
+  imports: [RoundBtnComponent, FormsModule, CommonModule],
   templateUrl: './add-channel-overlay.component.html',
   styleUrl: './add-channel-overlay.component.scss'
 })
@@ -15,6 +16,9 @@ export class AddChannelOverlayComponent implements OnInit {
 
   @Output() close = new EventEmitter<void>();
   @Output() createdChannel = new EventEmitter<any>();
+
+  @Input() channels: any[] = [];
+
 
   channelName: string = '';
   description: string = '';
@@ -33,20 +37,28 @@ export class AddChannelOverlayComponent implements OnInit {
     this.close.emit();
   }
 
+  get channelNameExists(): boolean {
+    const name = this.channelName.trim().toLowerCase();
+    return this.channels.some(c => c.name.trim().toLowerCase() === name);
+  }
+
+
   handleAddChannel() {
-    if (!this.currentUser) return;
+    if (!this.currentUser || this.channelNameExists) return;
+
     const newChannel = {
-      name: this.channelName,
+      name: this.channelName.trim(),
       description: this.description,
       createdBy: this.currentUser.uid,
       createdByName: this.currentUser.name,
       participants: [this.currentUser.uid],
       createdAt: new Date()
     };
+
     this.channelService.addChannel(newChannel).then(channelRef => {
-      const channelWithId = { id: channelRef.id, ...newChannel };
-      this.createdChannel.emit(channelWithId);
+      this.createdChannel.emit({ id: channelRef.id, ...newChannel });
       this.handleClose();
     });
   }
+
 }
