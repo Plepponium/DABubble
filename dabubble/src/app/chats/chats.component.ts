78faf42d65641863d272e1ddf263cda824c9fe61
CarlaHoffmann <carlahoffmann@gmx.de>
2026-01-19 13:10:29 +0100

    added show only channels with currentUser

--- dabubble/src/app/chats/chats.component.ts
+++ dabubble/src/app/chats/chats.component.ts
@@ -377,7 +377,7 @@ export class ChatsComponent implements OnInit, OnChanges {
   async toggleReaction(chatIndex: number, reactionType: string) {
     const chat = await this.getChatByIndex(chatIndex);
     if (!chat) return;
-    // console.log(chatIndex, reactionType);
+    
     const currentUsers = this.extractUserIds(chat.reactions || {}, reactionType);
     let updatedUsers: string[];
     if (currentUsers.includes(this.currentUserId)) {
