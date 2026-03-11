4fb69a248bd6377c46a9128b8bc3de1b068cca85
Plepponium <kleinchmida@gmail.com>
2026-03-11 19:56:48 +0100

    fix: improve email validation

--- dabubble/src/app/login/login.component.ts
+++ dabubble/src/app/login/login.component.ts
@@ -174,10 +174,7 @@ export class LoginComponent implements OnInit {
    */
   private createForm(): FormGroup {
     return this.fb.group({
-      email: ['', [
-        Validators.required,
-        Validators.pattern(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/)
-      ]],
+      email: ['', [Validators.required, Validators.email, Validators.pattern(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)]],
       password: ['', Validators.required]
     });
   }
